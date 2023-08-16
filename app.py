bb885ff40f11070fe71613983ae6d0457468669f
Gregor Hastings <gregor.hastings@flexitricity.com>
2023-08-16 23:30:28 +0100

    Began adding add screen

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from routes import routes
 
 app = Flask(__name__)
 app.register_blueprint(routes)
+app.secret_key = 'GrapesNotLemonade43'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 
 routes = Blueprint("routes", __name__, template_folder="templates")
 
@@ -10,14 +10,34 @@ def index():
 
 @routes.route("/incomes/manage")
 def manage_incomes():
-    return render_template("manage-incomes.html", pageTitle="Manage Incomes")
+    return render_template("incomes/manage-incomes.html", pageTitle="Manage Incomes")
+
+
+@routes.route("/incomes/add", methods=["GET", "POST"])
+def add_incomes():
+    if request.method == "POST":
+        flash("HIT")
+        return redirect(url_for("routes.add_incomes"))
+    return render_template("incomes/add-incomes.html", pageTitle="Add Income")
 
 
 @routes.route("/outgoings/manage")
 def manage_outgoings():
-    return render_template("manage-outgoings.html", pageTitle="Manage Outgoings")
+    return render_template("outgoings/manage-outgoings.html", pageTitle="Manage Outgoings")
+
+
+@routes.route("/outgoings/add")
+def add_outgoings():
+    return render_template("outgoings/add-outgoings.html", pageTitle="Add Outgoings")
 
 
 @routes.route("/savings-and-investments/manage")
 def manage_savings_and_investments():
-    return render_template("manage-savings-and-investments.html", pageTitle="Manage Savings and Investments")
+    return render_template("savings-and-investments/manage-savings-and-investments.html",
+                           pageTitle="Manage Savings and Investments")
+
+
+@routes.route("/savings-and-investments/add")
+def add_savings_and_investments():
+    return render_template("savings-and-investments/add-savings-and-investments.html",
+                           pageTitle="Add Savings and Investments")
