3c99c720ea15bd67c08119e2ab08d6ff47c36b9a
Gregor <gregor.hastings@outlook.com>
2023-08-19 10:12:07 +0100

    Reworked project

--- routes.py
+++ routes.py
@@ -1,43 +1,43 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask import Blueprint, render_template, redirect, url_for
 
 routes = Blueprint("routes", __name__, template_folder="templates")
 
 
 @routes.route("/")
 def index():
-    return render_template("index.html", pageTitle="Home")
+    return redirect(url_for("routes.home"))
+
+
+@routes.route("/home")
+def home():
+    return render_template("home.html", pageTitle="Home")
+
+
+@routes.route("/incomes/add")
+def add_incomes():
+    return render_template("add.html", pageTitle="Add Incomes")
 
 
 @routes.route("/incomes/manage")
 def manage_incomes():
-    return render_template("incomes/manage-incomes.html", pageTitle="Manage Incomes")
-
-
-@routes.route("/incomes/add", methods=["GET", "POST"])
-def add_incomes():
-    if request.method == "POST":
-        flash("HIT")
-        return redirect(url_for("routes.add_incomes"))
-    return render_template("incomes/add-incomes.html", pageTitle="Add Income")
-
-
-@routes.route("/outgoings/manage")
-def manage_outgoings():
-    return render_template("outgoings/manage-outgoings.html", pageTitle="Manage Outgoings")
+    return render_template("manage.html", pageTitle="Manage Incomes")
 
 
 @routes.route("/outgoings/add")
 def add_outgoings():
-    return render_template("outgoings/add-outgoings.html", pageTitle="Add Outgoings")
+    return render_template("add.html", pageTitle="Add Outgoings")
 
 
-@routes.route("/savings-and-investments/manage")
-def manage_savings_and_investments():
-    return render_template("savings-and-investments/manage-savings-and-investments.html",
-                           pageTitle="Manage Savings and Investments")
+@routes.route("/outgoings/manage")
+def manage_outgoings():
+    return render_template("manage.html", pageTitle="Manage Outgoings")
 
 
 @routes.route("/savings-and-investments/add")
 def add_savings_and_investments():
-    return render_template("savings-and-investments/add-savings-and-investments.html",
-                           pageTitle="Add Savings and Investments")
+    return render_template("add.html", pageTitle="Add Savings and Investments")
+
+
+@routes.route("/savings-and-investments/manage")
+def manage_savings_and_investments():
+    return render_template("manage.html", pageTitle="Manage Savings and Investments")
